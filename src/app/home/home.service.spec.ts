d09b5f2c0ff6281eba4112275112c48f0d98ce65
Jesiel Faria <fariajesiel@gmail.com>
2025-04-29 18:40:48 -0300

    Correção dos erros dos testes unitários

--- src/app/home/home.service.spec.ts
+++ src/app/home/home.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeService } from './home.service';
 
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(HomeService);
   });
 
