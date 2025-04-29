4f250899679c6c766da52aba91f3d4fecc0b050f
Jesiel Faria <fariajesiel@gmail.com>
2025-04-29 19:04:56 -0300

    Implementando teste unitário do home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
+import { CardModel } from '../components/card/model/card.model';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -18,5 +19,121 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve testar o receiveSearch() retornando o Card', () => {
+    const mockCards: CardModel[] = [
+      {
+        title: 'Card1',
+        description: 'Description 1',
+        img: 'link.imagem',
+        type: 1,
+        removed: false,
+      },
+      {
+        title: 'Card2',
+        description: 'Description 2',
+        img: 'link.imagem',
+        type: 2,
+        removed: false,
+      },
+    ];
+
+    component.listCards = mockCards;
+    component.receiveSearch('Card1');
+    expect(component.filteredCards).toEqual([
+      {
+        title: 'Card1',
+        description: 'Description 1',
+        img: 'link.imagem',
+        type: 1,
+        removed: false,
+      },
+    ]);
+  });
+
+  it('deve testar o receiveSearch() com Card1 Removido', () => {
+    const mockCards: CardModel[] = [
+      {
+        title: 'Card1',
+        description: 'Description 1',
+        img: 'link.imagem',
+        type: 1,
+        removed: true,
+      },
+      {
+        title: 'Card2',
+        description: 'Description 2',
+        img: 'link.imagem',
+        type: 2,
+        removed: false,
+      },
+    ];
+
+    component.listCards = mockCards;
+    component.receiveSearch('Card');
+    expect(component.filteredCards).toEqual([
+      {
+        title: 'Card2',
+        description: 'Description 2',
+        img: 'link.imagem',
+        type: 2,
+        removed: false,
+      },
+    ]);
+  });
+
+  it('deve testar o receiveSearch() com Search sem texto', () => {
+    const mockCards: CardModel[] = [
+      {
+        title: 'Card1',
+        description: 'Description 1',
+        img: 'link.imagem',
+        type: 1,
+        removed: false,
+      },
+      {
+        title: 'Card2',
+        description: 'Description 2',
+        img: 'link.imagem',
+        type: 2,
+        removed: false,
+      },
+    ];
+
+    component.listCards = mockCards;
+    component.receiveSearch('');
+
+    expect(component.filteredCards.length).toBe(2);
+    expect(component.filteredCards[0].title).toBe('Card1');
+    expect(component.filteredCards[1].title).toBe('Card2');
+  });
+
+  it('deve testar o removeCard()', () => {
+    const mockCards: CardModel[] = [
+      {
+        title: 'Card1',
+        description: 'Description 1',
+        img: 'link.imagem',
+        type: 1,
+        removed: false,
+      },
+      {
+        title: 'Card2',
+        description: 'Description 2',
+        img: 'link.imagem',
+        type: 2,
+        removed: false,
+      },
+    ];
+
+    component.listCards = mockCards;
+    component.filteredCards = [...component.listCards];
+
+    component.removeCard(0);
+
+    expect(component.listCards[0].removed).toBeTrue();
+    expect(component.filteredCards.length).toBe(1);
+    expect(component.filteredCards[0].title).toBe('Card2');
   });
 });
